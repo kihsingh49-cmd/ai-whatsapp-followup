62841cc04984a74290002a3e2f87c47da2c62b5e
kihsingh49-cmd <kihsingh49@gmail.com>
2026-09-07 16:56:25 +0530

    Change webhook server port to 10000

--- server/index.js
+++ server/index.js
@@ -38,4 +38,4 @@ app.post('/webhook', async (req, res) => {
   return res.sendStatus(200);
 });
 
-app.listen(process.env.PORT || 3001, () => console.log('Webhook server listening on port 1000'));
+app.listen(process.env.PORT || 10000, () => console.log('Webhook server listening on port 10000'));
